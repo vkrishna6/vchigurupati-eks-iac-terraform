d4c72038abd1fe35f7560989215dddc536983526
vamsikrishna <vamsikrishnach3@gmail.com>
2025-04-16 15:28:08 -0400

    Created route tables for both private, public subnets, NAT GW with EIP associated with one public subnet. Route table associated with private subnet has NAT gateway route.

--- main.tf
+++ main.tf
@@ -14,8 +14,9 @@ provider "aws" {
   region = var.region
 }
 
+# Create a VPC with "10.0.0.0/16" CIDR block.
 resource "aws_vpc" "test-vpc" {
-  cidr_block       = "10.0.0.0/24"
+  cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
   
   tags = {
@@ -24,6 +25,7 @@ resource "aws_vpc" "test-vpc" {
   }
 }
 
+# Create a public subnet with the CIDR range and availability_zones defined in variables.tf file. 
 resource "aws_subnet" "public" {
   count                   = length(var.public_subnet_cidrs)
   vpc_id                  = aws_vpc.test-vpc.id
@@ -36,6 +38,7 @@ resource "aws_subnet" "public" {
   }
 }
 
+# Create a Private subnet with the CIDR range and availability_zones defined in variables.tf file. 
 resource "aws_subnet" "private" {
   count             = length(var.private_subnet_cidrs)
   vpc_id            = aws_vpc.test-vpc.id
@@ -45,4 +48,72 @@ resource "aws_subnet" "private" {
   tags = {
     Name = "private-subnet-${count.index + 1}"
   }
+}
+
+# Create a IGW
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.test-vpc.id
+
+  tags = {
+    Name = "test-vpc-igw"
+  }
+}
+
+# Create a route table for public subnets - routetable1
+resource "aws_route_table" "routetable1" {
+  vpc_id = aws_vpc.test-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "routetable1-route-table"
+    subnets_included = "public"
+  }
+}
+
+# Route Table - routetable1 association with Public Subnets
+resource "aws_route_table_association" "routetable1" {
+  count          = length(aws_subnet.public)
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.routetable1.id
+}
+
+# Create elastic IP 
+resource "aws_eip" "nat_eip" {
+  domain = "vpc"
+}
+
+#Create a NAT gateway
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = aws_subnet.public[0].id  # it will Use first public subnet.
+
+  tags = {
+    Name = "test-vpc-nat-gateway"
+  }
+}
+
+#Create a private route table and mention the nat gateway id.
+resource "aws_route_table" "routetable2" {
+  vpc_id = aws_vpc.test-vpc.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat.id
+  }
+
+  tags = {
+    Name = "routetable2-route-table"
+    subnets_included = "private"
+  }
+}
+
+#associate private subnets to routing table created with NAT gateway route.
+resource "aws_route_table_association" "routetable2" {
+  count          = length(aws_subnet.private)
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.routetable2.id
 }
