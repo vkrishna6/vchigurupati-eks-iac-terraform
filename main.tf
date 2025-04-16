6e1af8bae47b3efb016cb765af164552e8ddd14d
vkrishna6 <52213383+vkrishna6@users.noreply.github.com>
2025-04-15 23:15:41 -0400

    eks cluster template from - "terraform-aws-eks" pub

--- main.tf
+++ main.tf
@@ -1,3 +1,5 @@
+# Reference - https://github.com/terraform-aws-modules/terraform-aws-eks
+
 terraform {
   required_providers {
     aws = {
@@ -9,10 +11,53 @@ terraform {
 
 # Configure the AWS Provider
 provider "aws" {
-  region = "us-east-1"
+  region = var.region
 }
 
-# Create a VPC
-#resource "aws_vpc" "example" {
-#  cidr_block = "10.0.0.0/16"
-#}
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.0"
+
+  cluster_name    = var.cluster_name
+  cluster_version = var.cluster_version
+
+  bootstrap_self_managed_addons = false
+  cluster_addons = {
+    coredns                = {}
+    eks-pod-identity-agent = {}
+    kube-proxy             = {}
+    vpc-cni                = {}
+  }
+
+  # Optional
+  cluster_endpoint_public_access = true
+
+  # Optional: Adds the current caller identity as an administrator via cluster access entry
+  enable_cluster_creator_admin_permissions = true
+
+  vpc_id                   = var.vpc_id
+  subnet_ids               = var.subnet_ids
+  #control_plane_subnet_ids = ["subnet-xyzde987", "subnet-slkjf456", "subnet-qeiru789"]
+
+  # EKS Managed Node Group(s)
+  eks_managed_node_group_defaults = {
+    instance_types = var.instance_types
+  }
+
+  eks_managed_node_groups = {
+    example = {
+      # Starting on 1.30, AL2023 is the default AMI type for EKS managed node groups
+      ami_type       = "AL2023_x86_64_STANDARD"
+      instance_types = var.instance_types
+
+      min_size     = 2
+      max_size     = 4
+      desired_size = 3
+    }
+  }
+
+  tags = {
+    Environment = "dev"
+    Terraform   = "true"
+  }
+}
--- variables.tf
+++ variables.tf
@@ -3,3 +3,28 @@ variable "region" {
   type        = string
   default     = "us-east-1"
 }
+
+variable "cluster_name" {
+  type        = string
+  default     = "test-cluster1"
+}
+
+variable "cluster_version" {
+  type        = number
+  default     = 1.32
+}
+
+variable "vpc_id" {
+  type        = string
+  default     = "vpc-0447c1683dab54521"
+}
+
+variable "subnet_ids" {
+  type        = list
+  default     = ["subnet-01cd7e89e203f4dce","subnet-0c1648c3eec6f733e","subnet-0d8334e53ad06e06b"]
+}
+
+variable "instance_types" {
+  type        = list
+  default     = ["m5.xlarge"]
+}
