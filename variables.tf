bca75447057abf132a4ba685450dc6fae43bf8dd
Vamsikrishna Chigurupati <52213383+vkrishna6@users.noreply.github.com>
2025-04-22 21:54:18 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,40 +1,40 @@
 variable "region" {
   description = "AWS region"
   type        = string
-  default     = "us-east-1"
+#  default     = "us-east-1"
 }
 
 variable "azs" {
   description = "Availability Zones"
   type        = list(string)
-  default     = ["us-east-1a", "us-east-1b", "us-east-1c"]
+#  default     = ["us-east-1a", "us-east-1b", "us-east-1c"]
 }
 
 variable "public_subnet_cidrs" {
   type        = list(string)
   description = "List of public subnet CIDRs"
-  default  = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+#  default  = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
 }
 
 variable "private_subnet_cidrs" {
   type        = list(string)
   description = "List of private subnet CIDRs"
-  default = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+#  default = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 }
 
 variable "cluster_name" {
   type        = string
-  default     = "test-cluster1"
+#  default     = "test-cluster1"
 }
 
 variable "cluster_version" {
   type        = string
   default     = "1.32"
-  description = "Kubernetes version for the EKS cluster"
+#  description = "Kubernetes version for the EKS cluster"
 }
 
 variable "instance_types" {
   type        = list(string)
   default     = ["m5.xlarge"]
-  description = "Instance types for the EKS managed node group"
+#  description = "Instance types for the EKS managed node group"
 }
