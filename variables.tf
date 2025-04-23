7d45e8ce07b5147d73405cd72370770bc494f50a
Vamsikrishna Chigurupati <52213383+vkrishna6@users.noreply.github.com>
2025-04-22 22:23:28 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,27 +1,3 @@
-variable "region" {
-  description = "AWS region"
-  type        = string
-#  default     = "us-east-1"
-}
-
-variable "azs" {
-  description = "Availability Zones"
-  type        = list(string)
-#  default     = ["us-east-1a", "us-east-1b", "us-east-1c"]
-}
-
-variable "public_subnet_cidrs" {
-  type        = list(string)
-  description = "List of public subnet CIDRs"
-#  default  = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-}
-
-variable "private_subnet_cidrs" {
-  type        = list(string)
-  description = "List of private subnet CIDRs"
-#  default = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-}
-
 variable "cluster_name" {
   type        = string
 #  default     = "test-cluster1"
