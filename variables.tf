f64dda6e0347e5e24619dac0326d93867c3f68f4
Vamsikrishna Chigurupati <52213383+vkrishna6@users.noreply.github.com>
2025-04-22 22:02:30 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -29,12 +29,12 @@ variable "cluster_name" {
 
 variable "cluster_version" {
   type        = string
-  default     = "1.32"
-#  description = "Kubernetes version for the EKS cluster"
+#  default     = "1.32"
+  description = "Kubernetes version for the EKS cluster"
 }
 
 variable "instance_types" {
   type        = list(string)
-  default     = ["m5.xlarge"]
-#  description = "Instance types for the EKS managed node group"
+#  default     = ["m5.xlarge"]
+  description = "Instance types for the EKS managed node group"
 }
