6c6179bdeb73dc29a16370794561339bce691090
vamsikrishna <vamsikrishnach3@gmail.com>
2025-04-23 21:34:55 -0400

    additional subnets added for 2nd cluster

--- variables.tf
+++ variables.tf
@@ -13,11 +13,11 @@ variable "azs" {
 variable "public_subnet_cidrs" {
   type        = list(string)
   description = "List of public subnet CIDRs"
-  default  = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  default  = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24", "10.0.4.0/24"]
 }
 
 variable "private_subnet_cidrs" {
   type        = list(string)
   description = "List of private subnet CIDRs"
-  default = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  default = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24", "10.0.104.0/24"]
 }
